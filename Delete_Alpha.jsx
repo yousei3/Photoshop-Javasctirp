218fdd61352e24e2d7d6461bbaae0a097cbb40f5
yousei3 <128758935+yousei3@users.noreply.github.com>
2023-03-30 05:53:54 +0900

    Add files via upload

---
+++ Delete_Alpha.jsx
@@ -0,0 +1,26 @@
+﻿//Turn off unneeded channels
+
+chNum = activeDocument.channels.length;
+//alert("チャンネル数は"+chNum+"です");
+ chNum=chNum-1;
+  colMode = activeDocument.mode;
+  
+//alert(colMode);  
+ if(colMode ==  "DocumentMode.RGB")   //RGB
+ {
+	for(i=1;2<chNum;chNum--)
+	{
+		aName = activeDocument.channels[chNum].name;
+	
+		activeDocument.channels[chNum].remove();
+	}
+}
+ if(colMode ==  "DocumentMode.CMYK")  //CMYKK
+ {
+	for(i=1;3<chNum;chNum--)
+	{
+		aName = activeDocument.channels[chNum].name;
+	
+		activeDocument.channels[chNum].remove();
+	}
+}
